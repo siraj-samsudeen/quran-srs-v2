562affe5ce27f765991e20f9ab25b23608213c55
Siraj Samsudeen <mailsiraj@gmail.com>
2021-07-01 09:23:53 +0530

    first unit test for checking home page url passes

--- srs_app/tests.py
+++ srs_app/tests.py
@@ -1,12 +1,8 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-
-class SmokeTest(TestCase):
-    def test(self):
-        assert 1 == 1
+from django.urls import resolve
+from srs_app.views import home_page
 
 
-def test_func():
-    assert 1 == 1
+def describe_home_page():
+    def url_is_valid():
+        assert resolve("/").func == home_page
